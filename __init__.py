bf3a5a16461fb502a2262426fb91097901b32aaf
davi133 <dqavi13@gmail.com>
2025-08-15 04:02:49 -0300

    fix init

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(f"{dir_path}\\dependencies")
 os.environ["SD_ENABLE_ASIO"] = "1"
-from .brush_sounds import *
+from .src.brush_sfx import *
 
 
 
