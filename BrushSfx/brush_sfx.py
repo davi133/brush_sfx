133af933625f76b5b613cba07893aedef011e946
davi133 <dqavi13@gmail.com>
2025-09-23 03:17:51 -0300

    volume slider

--- BrushSfx/brush_sfx.py
+++ BrushSfx/brush_sfx.py
@@ -1,5 +1,5 @@
 from krita import *
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QCheckBox, QComboBox, QLabel, QDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QCheckBox, QComboBox, QLabel, QDialog, QSlider
 from PyQt5.QtCore import Qt, pyqtSignal, QObject, QEvent, QTimer, QPoint
 from PyQt5.QtGui import QCursor, QGuiApplication
 import time
@@ -44,6 +44,13 @@ class BrushSFXExtension(Extension):
             _sound_choice_setting = "pencil-1"
         self.switchSoundChoice(_sound_choice_setting)
 
+        _volume_setting = Krita.instance().readSetting("BrushSfxGroup", "volume", "100")
+        if _volume_setting.isdigit():
+            _volume_setting = clamp(int(_volume_setting), 0, 100)
+        else:
+            _volume_setting = 50
+        self.changeVolume(int(_volume_setting))
+
 
     def setup(self):
         pass
@@ -63,6 +70,31 @@ class BrushSFXExtension(Extension):
         self.SFX_checkbox = QCheckBox("SFX", self.dialogWidget)
         self.SFX_checkbox.stateChanged.connect(self.switchOnOff)
 
+        # Volume slider
+        # label
+        volume_label = QLabel("Volume:", self.dialogWidget)
+        volume_label.setFixedWidth(43)
+        # slider
+        self.volume_slider = QSlider(self.dialogWidget)
+        self.volume_slider.setTracking(True)
+        self.volume_slider.setOrientation(Qt.Horizontal)
+        self.volume_slider.setMinimum(0)
+        self.volume_slider.setMaximum(100)
+        self.volume_slider.setTickInterval(10)
+        self.volume_slider.valueChanged.connect(self.changeVolume)
+        # value label 
+        self.volume_value_label = QLabel("100", self.dialogWidget)
+        self.volume_value_label.setFixedWidth(20)
+        # layout
+        volume_slider_layout = QHBoxLayout()
+        volume_slider_layout.addWidget(self.volume_slider)
+        volume_slider_layout.addWidget(self.volume_value_label)
+        
+
+        volume_layout = QVBoxLayout()
+        volume_layout.addWidget(volume_label)
+        volume_layout.addLayout(volume_slider_layout)
+
         # Sound Choice
         # label
         choice_label = QLabel("Sound Choice:", self.dialogWidget)
@@ -79,12 +111,21 @@ class BrushSFXExtension(Extension):
 
         self.dialogWidget.setLayout(main_layout)
         self.dialogWidget.layout().addWidget(self.SFX_checkbox)
+        self.dialogWidget.layout().addLayout(volume_layout)
         self.dialogWidget.layout().addLayout(choice_layout)
     
-    def setupDialog(self):
+    def setupDialogData(self):
         _checked_state_setting = Krita.instance().readSetting("BrushSfxGroup", "brush_sfx_on", "True")
         _qt_checked_state = Qt.Checked if _checked_state_setting != "False" else Qt.Unchecked
         self.SFX_checkbox.setCheckState(_qt_checked_state)
+
+        _volume_setting = Krita.instance().readSetting("BrushSfxGroup", "volume", "50")
+        print("volume setting", _volume_setting)
+        if _volume_setting.isdigit():
+            _volume_setting = clamp(int(_volume_setting), 0, 100)
+        else:
+            _volume_setting = 50
+        self.volume_slider.setValue(int(_volume_setting))
 
         _sound_choice_setting = Krita.instance().readSetting("BrushSfxGroup", "sound_choice", "pencil-1")
         if not _sound_choice_setting in self.__sound_options:
@@ -99,12 +140,20 @@ class BrushSFXExtension(Extension):
             Krita.instance().writeSetting("BrushSfxGroup", "brush_sfx_on", "False")
             self.input_listener.stopListening()
     
+    def changeVolume(self, value: int):
+        Krita.instance().writeSetting("BrushSfxGroup", "volume", str(value))
+        self.volume_value_label.setText(str(value))
+        volume = int(value)/100.0
+        self.player.setVolume(volume)
+
     def switchSoundChoice(self, new_text):
         Krita.instance().writeSetting("BrushSfxGroup", "sound_choice", new_text)
         self.player.setSoundSource(self.__sound_options[new_text])
+    
+    
 
     def openConfig(self):
-        self.setupDialog()
+        self.setupDialogData()
         self.dialogWidget.show()     
 
 # And add the extension to Krita's list of extensions:
